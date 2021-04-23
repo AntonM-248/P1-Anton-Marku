545145cfc6b2d8979e089f29dc05a207a0495f79
AntonM-248 <53975792+AntonM-248@users.noreply.github.com>
2021-04-23 09:27:45 -0400

    Add project files.

---
+++ RevaP1/Data/RevaP1DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevaP1.Data
+{
+    public class RevaP1DbContext: DbContext
+    {
+        public RevaP1DbContext(DbContextOptions<RevaP1DbContext> options): base(options)
+        {
+
+        }
+
+        //create dbsets for all your classes below
+
+    }
+}
---
+++ RevaP1/Models/Store.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevaP1.Models
+{
+    public class Store
+    {
+    }
+}
